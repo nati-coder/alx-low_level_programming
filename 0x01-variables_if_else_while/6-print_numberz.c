07572e8ae2474802761469c4bfb6da29d3a49af0
nati-coder <natnial99@gmail.com>
2022-04-08 12:38:27 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
- 
 #include <stdio.h>
 
 /**
- *  * main - Prints all one digit decimal number..
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Prints all one digit decimal number..
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
