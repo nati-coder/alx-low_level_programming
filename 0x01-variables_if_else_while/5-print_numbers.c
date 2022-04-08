62b0cbddc127cb3f0ea945216c6f2e7a947e7f29
nati-coder <natnial99@gmail.com>
2022-04-08 12:03:12 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all one digit decimal number..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf(i);
+	printf('\n');
+	return (0);
+}
