992da8a4c44a8fe1a2b321e389fd8b5f53359250
nati-coder <natnial99@gmail.com>
2022-04-08 12:28:05 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+ 
+#include <stdio.h>
+
+/**
+ *  * main - Prints all one digit decimal number..
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
