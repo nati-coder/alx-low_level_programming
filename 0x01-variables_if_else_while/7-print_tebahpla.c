ac40becbfd6096621514296b18996e30aafc1eb3
nati-coder <natnial99@gmail.com>
2022-04-08 12:43:38 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet reverse..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+        for (i = 25; i >= 0; i--)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+	return (0);
+}
