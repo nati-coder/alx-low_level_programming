b65bad4933b4522fb8903ec402814a89e6c41eb7
nati-coder <natnial99@gmail.com>
2022-04-12 12:26:54 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-	char put[8] = "_putchar";
-
-	_putchar(put[8]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
