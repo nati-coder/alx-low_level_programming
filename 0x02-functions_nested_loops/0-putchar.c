ffca7442da8863a849903c3dfa0b4842f92cc6b1
nati-coder <natnial99@gmail.com>
2022-04-12 12:43:52 -0700

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
+ * main - Prints _putchar..
  *
  * Return: Always 0 (Success)
  */
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet..
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = {'abcdefghijklmnopqrstuvwxyz'};
+
+	for(int i = 0; i < 26; i++)
+	{
+		_putchar(alph[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
