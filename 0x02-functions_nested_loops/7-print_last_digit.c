2a02cb922b299d8a6ccb9f329bb19882efd01d80
nati-coder <natnial99@gmail.com>
2022-04-12 14:18:12 -0700

    the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - Print the last digit
+ * of a number.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: The last digit.
+ */
+int print_last_digit(int i)
+{
+	int last;
+
+	last = i % 10;
+	return last;
+}
