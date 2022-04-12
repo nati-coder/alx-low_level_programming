92da64ac27ea9d3f3b448054a901f3feb8a0d23b
nati-coder <natnial99@gmail.com>
2022-04-12 13:35:12 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,6 @@
 #include "main.h"
 
 /**
- * main - Prints alphabets in lowercase 10 times.
- *
- * Return: Always 0 (Success)
  * print_alphabet_x10 - Print alphabet, in lowercases 10 x, end with new line
  */
 void print_alphabet_x10(void)
