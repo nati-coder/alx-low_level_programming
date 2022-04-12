10189cfa21a9a028f732981ad97bc8202b53418a
nati-coder <natnial99@gmail.com>
2022-04-12 13:37:59 -0700

    is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Shows 1 if the input is a
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
