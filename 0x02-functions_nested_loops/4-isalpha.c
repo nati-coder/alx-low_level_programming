7349f9414f3016ac09cf2b2f0c2005ae9fd8e7aa
nati-coder <natnial99@gmail.com>
2022-04-12 13:45:13 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Shows 1 if the input is alphabetic character. Another cases, shows 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for alphabetic character. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
