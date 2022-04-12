d564ba1ce965d614c15f2450948cadf00e12f219
nati-coder <natnial99@gmail.com>
2022-04-12 13:00:30 -0700

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
 /**
  * main - Prints the alphabet..
  *
- * Return: Always 0 (Success)
- * print_alphabet: print the alphabet.
+ * Return: Always 0 (Success).
  */
+/* print_alphabet: prints alphabet in lowercase*/
 void print_alphabet(void)
 {
 	int i = 0;
